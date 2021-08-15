3e5012a10767db88aef0843d2d9f1c9c96281ce6
Abiraamii <20eucs002@skcet.ac.in>
2021-08-15 09:17:18 +0530

    login and signup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-//import 'package:womentr/screens/LoginScreen.dart';
-
+import 'package:womentr/screens/LoginScreen.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: LoginScreen(),      
+      home: LoginScreen(),
     );
   }
 }
