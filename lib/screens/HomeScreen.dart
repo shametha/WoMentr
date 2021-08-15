579b173949e207bc053997c55710449093894d28
Shametha K G <shamethagopinathan@gmail.com>
2021-08-15 08:11:49 +0530

    initial commit

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -2,9 +2,9 @@ import 'package:womentr/SizeConfig.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:womentr/screens/Profile.dart';
-import 'package:womentr/screens/Question.dart';
-import 'package:womentr/screens/QuestionCategory.dart';
+// import 'package:womentr/screens/Profile.dart';
+// import 'package:womentr/screens/Question.dart';
+// import 'package:womentr/screens/QuestionCategory.dart';
 import '../../AppTheme.dart';
 
 class HomeScreen extends StatefulWidget {
